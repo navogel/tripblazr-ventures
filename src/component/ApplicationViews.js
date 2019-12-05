f386f1de3e53d533c04d632b1102f7eada9fd2dc
Nate V <navogel@gmail.com>
2019-12-05 17:16:10 -0600

    removed unused code from application views

--- src/component/ApplicationViews.js
+++ src/component/ApplicationViews.js
@@ -10,45 +10,13 @@ class ApplicationViews extends Component {
 		//OwnerView: false
 	};
 
-	// setOwner = () => {
-	// 	this.setState({ ownerView: true });
-	// };
-
-	// removeOwner = () => {
-	// 	this.setState({ ownerView: false });
-	// };
-
-	//  LATER: put this login into another component in order to match trip userID with active user
-	//
-	// componentDidMount() {
-	// 	TripManager.getTripDetails(parseInt(this.props.match.params.tripId)).then(
-	// 		trip => {
-	// 			this.setState({
-	// 				loaded: true,
-	// 				tripData: trip
-	// 			});
-	// 		}
-	// 	);
-	// 	console.log('app views did mount', this.state.tripData);
-	// }
+	
 
 	render() {
 		//console.log('app view render props', this.props);
 		return (
 			<React.Fragment>
-				{/* <Route
-					exact
-					path='/mytriplist'
-					render={props => {
-						return (
-							<ArticlesList
-								{...props}
-								// getUser={this.props.getUser}
-								activeUser={this.props.activeUser}
-							/>
-						);
-					}}
-				/> */}
+				
 
 				<Route
 					exact
@@ -91,37 +59,7 @@ class ApplicationViews extends Component {
 					}}
 				/>
 
-				{/* {this.state.loaded && (
-					<Route
-						exact
-						path='/mytrips/:tripId(\d+)'
-						render={props => {
-							if (this.state.tripData.id) {
-								console.log('its a real trip!');
-								if (this.props.user) {
-									console.log('got a user');
-									if (this.props.activeUser === this.state.tripData.userId) {
-										console.log('active user matches trip');
-										return (
-											<Trip
-												tripId={parseInt(props.match.params.tripId)}
-												activeUser={this.props.activeUser}
-												{...props}
-											/>
-										);
-									} else {
-										return <Redirect to='/login' />;
-									}
-								} else {
-									return <Redirect to='/login' />;
-								}
-							} else {
-								console.log('not a real trip');
-								return <Redirect to='/login' />;
-							}
-						}}
-					/>
-				)} */}
+				
 			</React.Fragment>
 		);
 	}
