96e5502f999911212f59a3e727c6b2701e93fce5
Nate V <navogel@gmail.com>
2020-01-03 18:30:18 -0600

    completed mobile tripview with styling

--- src/component/ApplicationViews.js
+++ src/component/ApplicationViews.js
@@ -1,85 +1,89 @@
 import { Route, Redirect, withRouter } from 'react-router-dom';
 import React, { Component } from 'react';
 import Trip from './TripView/TripView';
+import MobileTrip from './TripView/MobileTripView';
 import TripList from './WorldView/WoldView';
 import MobileTripList from './WorldView/MobileWorldView';
-import {isMobile} from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 
 class ApplicationViews extends Component {
-	state = {
-		loaded: false,
-		tripData: {}
-		//OwnerView: false
-	};
+    state = {
+        loaded: false,
+        tripData: {}
+        //OwnerView: false
+    };
 
-	
+    render() {
+        //console.log('app view render props', this.props);
+        console.log(isMobile);
+        return (
+            <React.Fragment>
+                <Route
+                    exact
+                    path='/mytrips'
+                    render={props => {
+                        if (this.props.user && isMobile) {
+                            console.log('mobile trip');
+                            return (
+                                <MobileTripList
+                                    {...props}
+                                    activeUser={this.props.activeUser}
+                                    clearUser={this.props.clearUser}
+                                    email={this.props.email}
+                                    // setOwner={this.setOwner}
+                                    // removeOwner={this.removeOwner}
+                                />
+                            );
+                        } else if (this.props.user) {
+                            console.log('reg trip');
+                            return (
+                                <TripList
+                                    {...props}
+                                    activeUser={this.props.activeUser}
+                                    clearUser={this.props.clearUser}
+                                    email={this.props.email}
+                                    // setOwner={this.setOwner}
+                                    // removeOwner={this.removeOwner}
+                                />
+                            );
+                        } else {
+                            return <Redirect to='/login' />;
+                        }
+                    }}
+                />
 
-	render() {
-		//console.log('app view render props', this.props);
-		console.log(isMobile);
-		return (
-			
-			<React.Fragment>
-				
-
-				<Route
-					exact
-					path='/mytrips'
-					render={props => {
-						if (this.props.user && isMobile) {
-							console.log('mobile trip')
-							return (
-								<MobileTripList
-									{...props}
-									activeUser={this.props.activeUser}
-									clearUser={this.props.clearUser}
-									email={this.props.email}
-									// setOwner={this.setOwner}
-									// removeOwner={this.removeOwner}
-								/>
-							);
-						} else if (this.props.user) {
-							console.log('reg trip')
-							return (
-								<TripList
-									{...props}
-									activeUser={this.props.activeUser}
-									clearUser={this.props.clearUser}
-									email={this.props.email}
-									// setOwner={this.setOwner}
-									// removeOwner={this.removeOwner}
-								/>
-							);
-						} else {
-							return <Redirect to='/login' />;
-						}
-					}}
-				/>
-
-				<Route
-					exact
-					path='/mytrips/:tripId(\d+)'
-					render={props => {
-						if (this.props.user) {
-							return (
-								<Trip
-									tripId={parseInt(props.match.params.tripId)}
-									activeUser={this.props.activeUser}
-									email={this.props.email}
-									//ownerView={this.state.ownerView}
-									{...props}
-								/>
-							);
-						} else {
-							return <Redirect to='/login' />;
-						}
-					}}
-				/>
-
-				
-			</React.Fragment>
-		);
-	}
+                <Route
+                    exact
+                    path='/mytrips/:tripId(\d+)'
+                    render={props => {
+                        if (this.props.user && isMobile) {
+                            return (
+                                <MobileTrip
+                                    tripId={parseInt(props.match.params.tripId)}
+                                    activeUser={this.props.activeUser}
+                                    email={this.props.email}
+                                    //ownerView={this.state.ownerView}
+                                    {...props}
+                                />
+                            );
+                        } else if (this.props.user) {
+                            return (
+                                <Trip
+                                    tripId={parseInt(props.match.params.tripId)}
+                                    activeUser={this.props.activeUser}
+                                    email={this.props.email}
+                                    //ownerView={this.state.ownerView}
+                                    {...props}
+                                />
+                            );
+                        } else {
+                            return <Redirect to='/login' />;
+                        }
+                    }}
+                />
+            </React.Fragment>
+        );
+    }
 }
 
 export default withRouter(ApplicationViews);
---
+++ src/component/TripViewComponents/MobileLocationCard.js
@@ -0,0 +1,174 @@
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+//import { firstLetterCase } from '../../modules/helpers';
+import TripManager from '../../modules/TripManager';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+//import CardMedia from '@material-ui/core/CardMedia';
+import '../WorldViewComponents/tripCard.css';
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+//table
+
+class MobileLocationCard extends Component {
+    state = { star: false };
+
+    componentDidMount() {
+        this.setState({ star: this.props.location.star });
+    }
+
+    handleDelete = id => {
+        TripManager.deleteLocation(id).then(() => this.props.getData());
+    };
+
+    addStar = (e, locationId) => {
+        e.stopPropagation();
+        let location = {
+            id: locationId,
+            star: true
+        };
+        TripManager.updateLocation(location);
+        this.setState({ star: true });
+    };
+
+    removeStar = (e, locationId) => {
+        e.stopPropagation();
+        let location = {
+            id: locationId,
+            star: false
+        };
+        TripManager.updateLocation(location);
+        this.setState({ star: false });
+    };
+
+    render() {
+        //console.log('hovered props', this.props.hovered);
+        return (
+            <>
+                <Card className='mobileTripCard' elevation={4}>
+                    <div className={'scroll' + this.props.location.id}></div>
+                    <CardActionArea
+                        onClick={() =>
+                            this.props.toggleLocDrawer(this.props.location)
+                        }
+                        className='cardActionArea'
+                    >
+                        {!this.props.publicTrip ? (
+                            <p className='cardLabel'>
+                                {this.props.location.locationType.locationType}
+
+                                {this.state.star ? (
+                                    <StarIcon
+                                        className='starred'
+                                        color='secondary'
+                                        onClick={e =>
+                                            this.removeStar(
+                                                e,
+                                                this.props.location.id
+                                            )
+                                        }
+                                    />
+                                ) : (
+                                    <StarBorderIcon
+                                        className='starred'
+                                        onClick={e =>
+                                            this.addStar(
+                                                e,
+                                                this.props.location.id
+                                            )
+                                        }
+                                    />
+                                )}
+                            </p>
+                        ) : (
+                            <p className='cardLabel'>
+                                {this.props.location.locationType.locationType}
+
+                                {this.state.star ? (
+                                    <StarIcon
+                                        className='starred'
+                                        color='secondary'
+                                        //onClick={e => this.removeStar(e, this.props.location.id)}
+                                    />
+                                ) : (
+                                    <StarBorderIcon
+                                        className='starred'
+                                        //onClick={e => this.addStar(e, this.props.location.id)}
+                                    />
+                                )}
+                            </p>
+                        )}
+                        {/* <Link to={`/mytrips/${this.props.location.id}`}> */}
+                        {/* <CardMedia
+								className='tripCardMedia'
+								image=
+								
+							/> */}
+
+                        <h3 className='mobile-card-tripname'>
+                            {/* {firstLetterCase(this.props.location.name)} */}
+                            {this.props.location.name}
+                        </h3>
+
+                        <p>{this.props.location.summary}</p>
+                    </CardActionArea>
+
+                    <CardActions
+                        className={
+                            'cardButtons' + this.props.location.locationTypeId
+                        }
+                    >
+                        <p className='price'>
+                            <b>
+                                {this.props.location.price &&
+                                    `$${this.props.location.price}`}
+                            </b>
+                        </p>
+                        {!this.props.publicTrip && (
+                            <>
+                                <Button
+                                    size='small'
+                                    color='primary'
+                                    onClick={() =>
+                                        this.handleDelete(
+                                            this.props.location.id
+                                        )
+                                    }
+                                >
+                                    Delete
+                                </Button>
+                                <Button
+                                    size='small'
+                                    color='primary'
+                                    // onClick={() => this.toggleDrawer(this.props.location)}
+                                    onClick={() =>
+                                        this.props.toggleDrawer(
+                                            this.props.location
+                                        )
+                                    }
+                                >
+                                    Edit
+                                </Button>
+                            </>
+                        )}
+                        <Button
+                            size='small'
+                            color='primary'
+                            // onClick={() => this.toggleDrawer(this.props.location)}
+                            onClick={() =>
+                                this.props.clickedCardItem(this.props.location)
+                            }
+                        >
+                            Fly to
+                        </Button>
+                    </CardActions>
+                </Card>
+            </>
+        );
+    }
+}
+
+export default MobileLocationCard;
