82d1ee2a6740aa3aa9d02224c5e175c378961871
Nate V <navogel@gmail.com>
2020-01-06 23:28:41 -0600

    reduced length of input fields to fit mobile

--- src/component/LocationComponents/AddNoteForm.js
+++ src/component/LocationComponents/AddNoteForm.js
@@ -15,172 +15,172 @@ import NativeSelect from '@material-ui/core/NativeSelect';
 import moment from 'moment';
 
 const styles = theme => ({
-	container: {
-		display: 'flex',
-		flexWrap: 'wrap'
-	},
-	textField: {
-		marginLeft: theme.spacing(1),
-		marginRight: theme.spacing(1)
-	},
-	dense: {
-		marginTop: 16
-	},
-	menu: {
-		width: 400
-	},
-	extendedIcon: {
-		marginRight: theme.spacing(1)
-	},
-	formControl: {
-		//margin: theme.spacing(1),
-		minWidth: 200
-	}
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap'
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1)
+    },
+    dense: {
+        marginTop: 16
+    },
+    menu: {
+        width: 400
+    },
+    extendedIcon: {
+        marginRight: theme.spacing(1)
+    },
+    formControl: {
+        //margin: theme.spacing(1),
+        minWidth: 200
+    }
 });
 
 class AddNoteForm extends React.Component {
-	state = {
-		// visible: false,
-		//locationId: '',
-		date: '',
-		note: '',
-		loadingStatus: false,
-		editTimeStamp: '',
-		type: 'note',
-		title: '',
-		labelWidth: 0
-	};
+    state = {
+        // visible: false,
+        //locationId: '',
+        date: '',
+        note: '',
+        loadingStatus: false,
+        editTimeStamp: '',
+        type: 'note',
+        title: '',
+        labelWidth: 0
+    };
 
-	componentDidMount() {
-		this.setState({
-			labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth
-		});
-	}
+    componentDidMount() {
+        this.setState({
+            labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth
+        });
+    }
 
-	handleFieldChange = evt => {
-		const stateToChange = {};
-		stateToChange[evt.target.id] = evt.target.value;
-		this.setState(stateToChange);
-	};
+    handleFieldChange = evt => {
+        const stateToChange = {};
+        stateToChange[evt.target.id] = evt.target.value;
+        this.setState(stateToChange);
+    };
 
-	handleChange = name => event => {
-		this.setState({ [name]: event.target.value });
-	};
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.value });
+    };
 
-	addNewNote = () => {
-		// evt.preventDefault();
-		this.setState({ loadingStatus: true });
-		if (this.state.message === '' || this.state.type === '') {
-			window.alert('Please fill out the note and type of note');
-		} else {
-			let userId = this.props.activeUser;
-			const message = {
-				locationId: this.props.locationId,
-				date: moment(new Date()),
-				note: this.state.note,
-				userId: userId,
-				editTimeStamp: '',
-				title: this.state.title,
-				type: this.state.type
-			};
-			TripManager.postLocationNote(message)
-				.then(this.props.getNotes)
-				.then(
-					this.setState({
-						date: '',
-						note: '',
-						loadingStatus: false,
-						editTimeStamp: '',
-						title: '',
-						type: ''
-					})
-				);
-			this.props.closeNewNote();
-		}
-	};
+    addNewNote = () => {
+        // evt.preventDefault();
+        this.setState({ loadingStatus: true });
+        if (this.state.message === '' || this.state.type === '') {
+            window.alert('Please fill out the note and type of note');
+        } else {
+            let userId = this.props.activeUser;
+            const message = {
+                locationId: this.props.locationId,
+                date: moment(new Date()),
+                note: this.state.note,
+                userId: userId,
+                editTimeStamp: '',
+                title: this.state.title,
+                type: this.state.type
+            };
+            TripManager.postLocationNote(message)
+                .then(this.props.getNotes)
+                .then(
+                    this.setState({
+                        date: '',
+                        note: '',
+                        loadingStatus: false,
+                        editTimeStamp: '',
+                        title: '',
+                        type: ''
+                    })
+                );
+            this.props.closeNewNote();
+        }
+    };
 
-	handleClick = evt => {
-		evt.preventDefault();
-		this.addNewMessage();
-		this.onClose();
-		document.querySelector('#note').value = '';
-	};
+    handleClick = evt => {
+        evt.preventDefault();
+        this.addNewMessage();
+        this.onClose();
+        document.querySelector('#note').value = '';
+    };
 
-	render() {
-		const { classes } = this.props;
+    render() {
+        const { classes } = this.props;
 
-		return (
-			<div className='msgSubmitRow'>
-				<div className='formField'>
-					<TextField
-						fullWidth
-						id='title'
-						label='Optional Title'
-						value={this.state.title}
-						onChange={this.handleFieldChange}
-						margin='dense'
-						variant='outlined'
-						placeholder='Optional Title'
-					/>
-					<TextField
-						fullWidth
-						id='note'
-						label='add a travel note, URL link, or Youtube video ID'
-						value={this.state.note}
-						onChange={this.handleFieldChange}
-						margin='dense'
-						variant='outlined'
-						placeholder='Add note'
-						multiline
-						rowsMax='4'
-					/>
-				</div>
-				<FormControl
-					variant='outlined'
-					margin='dense'
-					className={classes.formControl}
-				>
-					<InputLabel
-						ref={ref => {
-							this.InputLabelRef = ref;
-						}}
-						htmlFor='outlined-type-native-simple'
-					>
-						Note Type
-					</InputLabel>
-					<NativeSelect
-						value={this.state.type}
-						onChange={this.handleChange('type')}
-						input={
-							<OutlinedInput
-								name='type'
-								labelWidth={this.state.labelWidth}
-								id='type'
-							/>
-						}
-					>
-						<option value={'note'}>{'Travel Note '}</option>
-						<option value={'url'}>{'Link '}</option>
-						<option value={'YT'}>{'YouTube Id '}</option>
-					</NativeSelect>
-				</FormControl>
+        return (
+            <div className='msgSubmitRow'>
+                <div className='formField'>
+                    <TextField
+                        fullWidth
+                        id='title'
+                        label='Optional Title'
+                        value={this.state.title}
+                        onChange={this.handleFieldChange}
+                        margin='dense'
+                        variant='outlined'
+                        placeholder='Optional Title'
+                    />
+                    <TextField
+                        fullWidth
+                        id='note'
+                        label='add a note, URL, or Youtube video ID'
+                        value={this.state.note}
+                        onChange={this.handleFieldChange}
+                        margin='dense'
+                        variant='outlined'
+                        placeholder='Add note'
+                        multiline
+                        rowsMax='4'
+                    />
+                </div>
+                <FormControl
+                    variant='outlined'
+                    margin='dense'
+                    className={classes.formControl}
+                >
+                    <InputLabel
+                        ref={ref => {
+                            this.InputLabelRef = ref;
+                        }}
+                        htmlFor='outlined-type-native-simple'
+                    >
+                        Note Type
+                    </InputLabel>
+                    <NativeSelect
+                        value={this.state.type}
+                        onChange={this.handleChange('type')}
+                        input={
+                            <OutlinedInput
+                                name='type'
+                                labelWidth={this.state.labelWidth}
+                                id='type'
+                            />
+                        }
+                    >
+                        <option value={'note'}>{'Travel Note '}</option>
+                        <option value={'url'}>{'Link '}</option>
+                        <option value={'YT'}>{'YouTube Id '}</option>
+                    </NativeSelect>
+                </FormControl>
 
-				<div className='noteSubmit'>
-					<Fab
-						variant='extended'
-						size='small'
-						color='primary'
-						aria-label='submit'
-						disabled={this.state.loadingStatus}
-						onClick={this.addNewNote}
-					>
-						<AddIcon />
-						Submit
-					</Fab>
-				</div>
-			</div>
-		);
-	}
+                <div className='noteSubmit'>
+                    <Fab
+                        variant='extended'
+                        size='small'
+                        color='primary'
+                        aria-label='submit'
+                        disabled={this.state.loadingStatus}
+                        onClick={this.addNewNote}
+                    >
+                        <AddIcon />
+                        Submit
+                    </Fab>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withStyles(styles)(AddNoteForm);
--- src/component/LocationComponents/LocationDetailsDrawer.js
+++ src/component/LocationComponents/LocationDetailsDrawer.js
@@ -5,132 +5,76 @@ import Drawer from '@material-ui/core/Drawer';
 import LocationDetails from './LocationDetails';
 
 const styles = {
-	list: {
-		width: 500
-	},
-	fullList: {
-		width: 'auto'
-	}
+    list: {
+        width: 500
+    },
+    fullList: {
+        width: 'auto'
+    }
 };
 
 class LocDrawer extends React.Component {
-	state = {
-		right: false,
-		location: {}
-	};
+    state = {
+        right: false,
+        location: {}
+    };
 
-	toggleLocDrawer = (side, open) => () => {
-		this.setState({
-			[side]: open
-		});
-	};
+    toggleLocDrawer = (side, open) => () => {
+        this.setState({
+            [side]: open
+        });
+    };
 
-	openLocDrawer = obj => {
-		this.setState({
-			right: true,
-			location: obj
-		});
-	};
+    openLocDrawer = obj => {
+        this.setState({
+            right: true,
+            location: obj
+        });
+    };
 
-	closeLocDrawer = () => {
-		this.setState({
-			right: false
-		});
-	};
+    closeLocDrawer = () => {
+        this.setState({
+            right: false
+        });
+    };
 
-	render() {
-		//const { classes } = this.props;
-
-		// const sideList = (
-		// 	<div className={classes.list}>
-		// 		<List>
-		// 			{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-		// 				<ListItem button key={text}>
-		// 					<ListItemIcon>
-		// 						{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-		// 					</ListItemIcon>
-		// 					<ListItemText primary={text} />
-		// 				</ListItem>
-		// 			))}
-		// 		</List>
-		// 		<Divider />
-		// 		<List>
-		// 			{['All mail', 'Trash', 'Spam'].map((text, index) => (
-		// 				<ListItem button key={text}>
-		// 					<ListItemIcon>
-		// 						{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-		// 					</ListItemIcon>
-		// 					<ListItemText primary={text} />
-		// 				</ListItem>
-		// 			))}
-		// 		</List>
-		// 	</div>
-		// );
-
-		// const fullList = (
-		// 	<div className={classes.fullList}>
-		// 		<List>
-		// 			{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-		// 				<ListItem button key={text}>
-		// 					<ListItemIcon>
-		// 						{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-		// 					</ListItemIcon>
-		// 					<ListItemText primary={text} />
-		// 				</ListItem>
-		// 			))}
-		// 		</List>
-		// 		<Divider />
-		// 		<List>
-		// 			{['All mail', 'Trash', 'Spam'].map((text, index) => (
-		// 				<ListItem button key={text}>
-		// 					<ListItemIcon>
-		// 						{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-		// 					</ListItemIcon>
-		// 					<ListItemText primary={text} />
-		// 				</ListItem>
-		// 			))}
-		// 		</List>
-		// 	</div>
-		// );
-
-		return (
-			<>
-				{/* <Button onClick={this.toggleDrawer('right', true)}>Open Right</Button> */}
-				{/* <ClickAwayListener onClickAway={this.closeDrawer}> */}
-				<Drawer
-					anchor='right'
-					open={this.state.right}
-					onClose={this.toggleLocDrawer('right', false)}
-				>
-					<div
-						tabIndex={0}
-						role='button'
-						// onClick={this.toggleDrawer('right', false)}
-						//onKeyDown={this.toggleDrawer('right', false)}
-					>
-						{/* {sideList} */}
-						<div className='drawerWrapper'>
-							<LocationDetails
-								location={this.state.location}
-								getData={this.props.getData}
-								closeLocDrawer={this.closeLocDrawer}
-								toggleDrawer={this.props.toggleDrawer}
-								activeUser={this.props.activeUser}
-								publicTrip={this.props.publicTrip}
-								tripId={this.props.tripId}
-								likes={this.props.likes}
-							/>
-						</div>
-					</div>
-				</Drawer>
-				{/* </ClickAwayListener> */}
-			</>
-		);
-	}
+    render() {
+        return (
+            <>
+                <Drawer
+                    anchor='right'
+                    open={this.state.right}
+                    onClose={this.toggleLocDrawer('right', false)}
+                >
+                    <div
+                        tabIndex={0}
+                        role='button'
+                        // onClick={this.toggleDrawer('right', false)}
+                        //onKeyDown={this.toggleDrawer('right', false)}
+                    >
+                        {/* {sideList} */}
+                        <div className='drawerWrapper'>
+                            <LocationDetails
+                                location={this.state.location}
+                                getData={this.props.getData}
+                                closeLocDrawer={this.closeLocDrawer}
+                                toggleDrawer={this.props.toggleDrawer}
+                                activeUser={this.props.activeUser}
+                                publicTrip={this.props.publicTrip}
+                                tripId={this.props.tripId}
+                                likes={this.props.likes}
+                            />
+                        </div>
+                    </div>
+                </Drawer>
+                {/* </ClickAwayListener> */}
+            </>
+        );
+    }
 }
 
 LocDrawer.propTypes = {
-	classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(LocDrawer);
